9d82205e2cc948284a6cb165c883b4ae5e465005
gkeswani92 <gauravkeswani92@gmail.com>
2015-10-20 21:59:59 -0400

    Find closure of a attribute set

--- src/BCNF.java
+++ src/BCNF.java
@@ -21,26 +21,49 @@ public class BCNF {
 		return Collections.emptySet();
 	}
 
-	
-	/**
-	 * Method to determine all the possible combinations of attributes
-	 * @param attributes
-	 * @param startIndex
-	 * @param attributeCombination
-	 * @param combinationGenerator
-	 * @return
-	 */
-	public static ArrayList<AttributeSet> getAttributeCombinations(ArrayList<Attribute> attributes, int startIndex,
-					ArrayList<AttributeSet> attributeCombination, AttributeSet combinationGenerator) {
-		
-		for (int i = startIndex; i < attributes.size(); i++) {
-			combinationGenerator.addAttribute(attributes.get(i));
-			attributeCombination.add(new AttributeSet(combinationGenerator));
-			attributeCombination = getAttributeCombinations(attributes, i+1, attributeCombination, combinationGenerator);
-			combinationGenerator.removeLastAttribute();
-		}
-		return attributeCombination;
-	}
-	
 
+	/**
+	 * Recommended helper method 
+	 * ￼CLOSURE(X, F) 
+	 * begin 
+	 * OLDDEP = null; 
+	 * NEWDEP = X,
+	 * while NEWDEP != OLDDEP do 
+	 * begin 
+	 * 		OLDDEP := NEWDEP; 
+	 * 		for every FD W->Z in F do 
+	 * 			if NEWDEP I> W then 
+	 * 				NEWDEP :=NEWDEP U Z 
+	 * 			end; 
+	 * retum(NEWDEP) 
+	 * end
+	 **/
+	public static AttributeSet closure(AttributeSet attributeSet, Set<FunctionalDependency> functionalDependencies) {
+
+		AttributeSet closureOfAttributeSet = new AttributeSet(attributeSet);
+		ArrayList<AttributeSet> attributeCombination = new ArrayList<AttributeSet>();
+		AttributeSet combinationGenerator = new AttributeSet();
+		ArrayList<AttributeSet> currentAttrCombination = getAttributeCombinations(attributeSet.getAttributeList(), 0, attributeCombination, combinationGenerator);
+		
+		//Determining the closure from the attribute set passed to the method
+		for(AttributeSet currentAS: currentAttrCombination){
+			for(FunctionalDependency fd: functionalDependencies){
+				if(fd.independent().equals(currentAS)){
+					closureOfAttributeSet.appendAttributeSet(fd.dependent());
+				}
+			}
+		}
+		
+		//Determining other attributes of the closure depending on the computer closure uptil now
+		currentAttrCombination = getAttributeCombinations(closureOfAttributeSet.getAttributeList(), 0, attributeCombination, combinationGenerator);
+		for(AttributeSet currentAS: currentAttrCombination){
+			for(FunctionalDependency fd: functionalDependencies){
+				if(fd.independent().equals(currentAS)){
+					closureOfAttributeSet.appendAttributeSet(fd.dependent());
+				}
+			}
+		}
+		
+		return closureOfAttributeSet;
+	}
 }
